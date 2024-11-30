7f4ef055df0d425a35bb3a2e89d874252e751734
sushant kachbawar <suk.sushant@gmail.com>
2024-11-30 23:12:25 +0530

    refactored code and added some more logic

--- Hangman.py
+++ Hangman.py
@@ -1,24 +1,29 @@
-#This code implements a simple word-guessing game where a random word is selected from a predefined list. 
-#The player guesses letters, and correctly guessed letters are revealed in their corresponding positions in the word, while the rest remain hidden.
-
-word_list = ["test", "ganesh", "sushant"]
-
+#This code implements a word-guessing game where a player attempts to guess a randomly chosen word one letter at a time. 
+#The game continues until the player correctly guesses all the letters in the word, revealing the complete word.
 import random
+word_list = ["test", "ganesh", "sushant"]
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
 print(f'Shhhh, the solution is {chosen_word}')
 
 display = []
 
-for _ in range(len(chosen_word)):
+for _ in range(word_length):
     display += "_"
-print(display)
 
-guess = input("Guess a letter :").lower()
+end_of_game = False
 
-for position in range(len(chosen_word)):
-    letter = chosen_word[position ]
-    if letter == guess:
-        display[position] = letter
+while not end_of_game:
+    guess = input("Guess a letter :").lower()
 
-print(display)
+    for position in range(word_length):
+        letter = chosen_word[position ]
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You Win")
