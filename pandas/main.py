0816139a1f21ec9012046258318a54f07d3f4a44
sushant kachbawar <suk.sushant@gmail.com>
2024-12-08 16:13:22 +0530

    added pandas learning and guess 50 state of US game

--- pandas/main.py
+++ pandas/main.py
@@ -1,3 +1,4 @@
+#1 pandas basics
 import pandas
 
 data = pandas.read_csv("2018_Central_Park_Squirrel_Census_Squirrel_Data.csv")
@@ -14,3 +15,41 @@ data_dict = {"fur_colour":["Gray", "Black", "Cinnamon" ], "count":[gray_s_count,
 df = pandas.DataFrame(data_dict)
 df.to_csv("squirrel_count.csv")
 
+#2 50 states game
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+state_data = pandas.read_csv("50_states.csv")
+all_states = state_data.state.to_list()
+guessed_state = []
+
+
+while len(guessed_state) < 50:
+
+    answer_state = screen.textinput(title=f"{len(guessed_state)}/50 States correct", prompt="what's another state name").title()
+    
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_state:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("States_to_Learn.csv")
+        print(missing_states)
+        break
+
+    if answer_state in all_states:
+        guessed_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        selected_state = state_data[state_data.state == answer_state]
+        t.goto(int(selected_state.x), int(selected_state.y))
+        t.write(answer_state)
